ce2f9e2bfcad3d1d045b694910e171082346330c
UnidentifiedSecTM <59448060+Himosis@users.noreply.github.com>
2020-02-28 11:10:09 +0330

    Create Devil034.sh

---
+++ Devil034.sh
@@ -0,0 +1,49 @@
+#!/system/xbin/bash
+#coded by Devil034
+
+############################################
+# CTRL + C                                 #
+############################################
+trap ctrl_c INT
+ctrl_c() {
+clear
+echo -b $green"[#]> (ctrl + c) Detected, trying to Exit...." | lolcat 
+echo -b $red"[#]> MahdiiHC " | lolcat
+echo -b $blue"[#]> SEE YOU AGAIN:)..." | lolcat
+sleep 1
+exit 
+}
+
+clear
+toilet -f mono12 -F gay "Hack028"
+sleep 3
+echo " in the name of Allah " | lolcat
+sleep 2
+echo "_______________"
+sleep 2
+echo " Coded By ~> Devil034 - Hacked16 " | lolcat
+sleep 2
+echo " github orginal : https://github.com/BullsEye0/google_dork_list "
+sleep 2
+echo " github : https://github.com/Himosis " | lolcat
+sleep 2
+echo " CHannel Tel ==> t.me/Hack028 " | lolcat
+sleep 2
+echo " ____________________ " | lolcat
+sleep 2
+echo " 99.Download dork Google " | lolcat
+
+
+read Devil034
+
+if
+[ $Devil034 = 99 ] || [ $Devil034 = 99 ]
+then
+clear
+pkg install git -y
+clear
+git clone https://github.com/BullsEye0/google_dork_list.git
+clear
+cd google_dork_list
+nano google_Dorks.txt
+fi
